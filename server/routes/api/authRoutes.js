bda1ccf134b0e79e9d116f787d0be00f1f78a828
MNlegion <rkreuser30@gmail.com>
2023-12-11 18:19:19 -0500

    updated register and login routes to include email from userModel.js

--- server/routes/api/authRoutes.js
+++ server/routes/api/authRoutes.js
@@ -6,40 +6,41 @@ const router = express.Router();
 
 // Register a new user
 router.post('/register', async (req, res) => {
-    try {
-      const { username, password } = req.body;
-      const existingUser = await User.findOne({ username });
-  
-      if (existingUser) {
-        return res.status(400).json({ message: 'Username already exists' });
-      }
-  
-      const newUser = new User({ username, password });
-      await newUser.save();
-  
-      res.status(201).json({ message: 'User registered successfully' });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
+  try {
+    const { username, email, password } = req.body;
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
 
-  router.post('/login', (req, res, next) => {
-    passport.authenticate('local', { session: false }, (err, user, info) => {
-      if (err) {
-        return res.status(500).json({ message: err.message });
-      }
-  
-      if (!user) {
-        return res.status(401).json({ message: info.message });
-      }
-  
-      const token = jwt.sign({ sub: user._id }, 'your-secret-key', {
-        expiresIn: '1h', // Token expires in 1 hour
-      });
-  
-      res.json({ token });
-    })(req, res, next);
-  });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Username or email already exists' });
+    }
+
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Login an existing user
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', { session: false }, (err, user, info) => {
+    if (err) {
+      return res.status(500).json({ message: err.message });
+    }
+
+    if (!user) {
+      return res.status(401).json({ message: info.message });
+    }
+
+    const token = jwt.sign({ sub: user._id }, 'your-secret-key', {
+      expiresIn: '1h', // Token expires in 1 hour
+    });
+
+    res.json({ token });
+  })(req, res, next);
+});
   
   router.get('/profile', passport.authenticate('jwt', { session: false }), (req, res) => {
     // The user is authenticated and req.user contains user information
